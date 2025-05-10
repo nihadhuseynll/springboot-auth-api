cb44cc99894eb0e19136211f65b724519c0d5944
Nikoli <huseynlinihad4400@gmail.com>
2025-05-10 10:56:12 +0400

    Initial commit

---
+++ src/main/java/com/example/springbootauthapi/SpringbootAuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootauthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAuthApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAuthApiApplication.class, args);
+    }
+
+}
